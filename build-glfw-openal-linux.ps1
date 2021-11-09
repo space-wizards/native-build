4314abc9d992757b1c62695c1019e5f630d4b194
20kdc <asdd2808@gmail.com>
2021-11-09 14:41:34 +0000

    Build libfluidsynth.so.2 (#1)

--- build-glfw-openal-linux.ps1
+++ build-glfw-openal-linux.ps1
@@ -5,6 +5,9 @@ docker run --rm -v "${pwd}:/opt" ss14-native-build:1.0 "/bin/bash" "/opt/__build
 
 new-item -Force -itemtype Directory builds/glfw/
 new-item -Force -itemtype Directory builds/openal/
+new-item -Force -itemtype Directory builds/fluidsynth/
 
 copy-item glfw/build/src/libglfw.so.3.3 builds/glfw/libglfw.so
 copy-item openal-soft/build/libopenal.so.1.20.1 builds/openal/libopenal.so
+copy-item fluidsynth/build/src/libfluidsynth.so.2.3.3 builds/fluidsynth/libfluidsynth.so
+
