80173d8f6d894b19f156192eedaadeba38959d6f
20kdc <asdd2808@gmail.com>
2022-04-14 21:31:56 +0100

    Go back to calling this container version "1.0"

--- linux/build-build-env.ps1
+++ linux/build-build-env.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
 
-docker build -t ss14-native-build:1.1 -f build-env.Dockerfile . 
+docker build -t ss14-native-build:1.0 -f build-env.Dockerfile . 
--- linux/build-opus.ps1
+++ linux/build-opus.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-docker run --rm -v "${pwd}/..:/opt" ss14-native-build:1.1 "/bin/bash" "/opt/linux/__build-opus.sh"
+docker run --rm -v "${pwd}/..:/opt" ss14-native-build:1.0 "/bin/bash" "/opt/linux/__build-opus.sh"
 
 new-item -Force -itemtype Directory ../builds/opus/
 copy-item build/opus/libopus.so.0 ../builds/opus/
