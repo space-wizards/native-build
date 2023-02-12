288144e417e833d38c20d8d0433c20a02a76b1a2
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2023-02-12 23:25:01 +0100

    Fix bad $(pwd) in linux build

--- linux/build-vcpkg.sh
+++ linux/build-vcpkg.sh
@@ -3,7 +3,7 @@
 TRIPLET=$1
 RID=$2
 
-docker run --rm -v "${pwd}/..:/opt" ss14-native-build:1.0 /opt/vcpkg/vcpkg install --triplet "$TRIPLET" "sdl2[vulkan]" "openal-soft" "freetype[core]" "glfw3"
+docker run --rm -v "$(pwd)/..:/opt" ss14-native-build:1.0 /opt/vcpkg/vcpkg install --triplet "$TRIPLET" "sdl2[vulkan]" "openal-soft" "freetype[core]" "glfw3"
 
 TO_COPY=("libSDL2-2.0.so.0" "libfreetype.so.6" "libglfw.so.3" "libopenal.so.1")
 
