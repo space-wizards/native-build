7f8e7f375a79dc3dd2d01370c16b64c246b32029
BarryNorfolk <barrynorfolkman@protonmail.com>
2025-02-14 17:46:25 +0100

    Add scripts for building on each major platform

---
+++ scripts/osx.py
@@ -0,0 +1,33 @@
+#!/bin/bash python3
+from software.zstd import ZStd
+from software.glfw import GLFW
+from software.sdl import SDL
+from software.openal import OpenAL
+from software.freetype import Freetype
+from software.fluidsynth import Fluidsynth
+
+from common import Github, Software, dump_build_notes, ARTIFACT_DIR
+
+from pathlib import Path
+
+if __name__ == "__main__":
+    to_build: list[Software] = [
+        ZStd(),
+        GLFW(),
+        SDL(),
+        OpenAL(),
+        Freetype(),
+        Fluidsynth(),
+    ]
+
+    for build in to_build:
+        with Github.LogGroup(f"Building {build.name}"):
+            build.build()
+            build.publish()
+
+    dump_build_notes(
+        "native-build (OSX x64)",
+        Path("."),
+        ARTIFACT_DIR.joinpath("notes.md"),
+        [f"- {build.name}" for build in to_build],
+    )
